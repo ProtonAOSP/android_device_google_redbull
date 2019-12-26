f21326d931b3482ebafe0dc8d54cb0880c8a0352
Nick Chung <nickchung@google.com>
2019-12-26 18:20:09 +0800

    device-common.mk: Enable hal buffer management
    Bug: 145725727
    Test: build pass, local verify.
    Change-Id: I0e58154274c799fe1bd9025e5121f82ce47309cd

--- device-common.mk
+++ device-common.mk
@@ -279,7 +279,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # camera hal buffer management
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.managebuffer.enable=0
+    persist.camera.managebuffer.enable=1
 
 # Lets the vendor library that Google Camera HWL is enabled
 PRODUCT_PROPERTY_OVERRIDES += \
