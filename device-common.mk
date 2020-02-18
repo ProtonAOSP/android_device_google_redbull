b9f3cfefa8bc54991d140f4fcc5d4c994e04e8cb
Robin Peng <robinpeng@google.com>
2020-02-18 12:22:19 +0800

    [Single RIL w/ SM7250] Amend vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims to vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims/ims-ext-common
    FAILED: vendor/qcom/sm7250/proprietary/commonsys/telephony-apps/ims/Android.mk: error: "ims (APPS android-arm64) missing ims-ext-common (JAVA_LIBRARIES android-arm64)"
    
    Bug: 149175890
    Change-Id: I277dc37e51262b19baa71d2a89023b7c32f1b0f0

--- device-common.mk
+++ device-common.mk
@@ -42,7 +42,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Single vendor RIL/Telephony/data with SM7250
   PRODUCT_SOONG_NAMESPACES += \
-      vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims \
+      vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims/ims-ext-common \
       vendor/qcom/sm7250/proprietary/qcril-data-hal \
       vendor/qcom/sm7250/proprietary/qcril-hal
 
