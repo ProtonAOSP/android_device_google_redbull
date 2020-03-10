b690de3c9645b3c5d952445beab7caba7b9cf9f7
Jason Chiu <jasoncschiu@google.com>
2020-03-10 20:58:58 +0800

    Disable Rescue Party on userdebug & eng build
    Bug: 151151366
    Change-Id: I9823e1b194f1b7fc86c6a26943d6256f804efc6e
    Signed-off-by: Jason Chiu <jasoncschiu@google.com>

--- device-common.mk
+++ device-common.mk
@@ -877,6 +877,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.verbose_logging_enabled=false
 endif
 
+# Disable Rescue Party on userdebug & eng build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+endif
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 #################################################################################
