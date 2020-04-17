1ad8b467e0b1c386cb292342afdfe60786d23eb5
Martin Liu <liumartin@google.com>
2020-04-17 18:49:24 +0800

    mm_event: add mm_event from common folder
    This CL add mm_event and relative sepolicy
    from common mk file.
    
    Bug: 152414692
    Test: build
    Change-Id: I15aa8d46026a5e2a8d4d5865c2619a3749b538d9

--- device-common.mk
+++ device-common.mk
@@ -960,4 +960,7 @@ include hardware/google/pixel/common/pixel-common-device.mk
 
 # power HAL
 -include hardware/google/pixel/power-libperfmgr/aidl/device.mk
+
+# mm_event
+-include hardware/google/pixel/mm/device.mk
 #################################################################################
