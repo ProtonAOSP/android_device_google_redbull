ed0775df3d6e0afbf930da8633ad44c30754a3fb
Mark Salyzyn <salyzyn@google.com>
2020-05-08 15:23:50 -0700

    redbull: do not load touchscreen drivers automatically
    Drop the pair of touchscreen drivers, one for each product, so
    that they are not loaded automatically with modprobe, but instead
    loaded manually as required by each platform.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 151950334
    Test: fastboot flashall, boot, and make sure touchscreen works.
    Merged-In: I668d7f414ceb56fa4b8c0a539b3515518955f72b
    Change-Id: I668d7f414ceb56fa4b8c0a539b3515518955f72b

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -223,3 +223,7 @@ BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 4873781248
 BOARD_SUPER_PARTITION_ERROR_LIMIT := 9231663104
 
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
+# List of modules that should not load automatically
+PRODUCT_COPY_FILES += \
+    device/google/redbull/modules.blacklist:$(TARGET_COPY_OUT_VENDOR)/lib/modules/modules.blacklist
