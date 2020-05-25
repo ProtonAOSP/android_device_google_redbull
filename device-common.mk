6d6096b497fdecaaf46b6e4d4dda4a7760a543af
Ellie Yang <shusyuan@google.com>
2020-05-25 16:40:53 +0800

    camera: Add camera.concurrent into supported feature
    Bug: 152354541
    Test: CTS
    Change-Id: I863031e44d669b7ce030f8c38ce0384eb9df66d8

--- device-common.mk
+++ device-common.mk
@@ -195,6 +195,7 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml\
