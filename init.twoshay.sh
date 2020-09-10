8e9105c1de288386a61f96313db9ba3de87cf33c
Steve Pfetsch <spfetsch@google.com>
2020-09-10 10:58:19 -0700

    redbull: disable twoshay study
    Remove conditional code that was used to perform selection for the
    twoshay study.
    
    Bug: 162882504
    Signed-off-by: Steve Pfetsch <spfetsch@google.com>
    Change-Id: I72a4cabdc0c5b9a424c39b9bacbd6f7592a89f60

--- init.twoshay.sh
+++
@@ -1,8 +0,0 @@
-#!/vendor/bin/sh
-
-# Implementation of A/B group selection - See b/161398758
-seedfile=/sys/devices/platform/soc/soc:qcom,dsi-display-primary/panel_info/panel0/serial_number
-sum=$(sha1sum $seedfile)
-result=$(expr $((16#${sum:1:1})) % 2)
-
-setprop vendor.twoshay.study_group $result
