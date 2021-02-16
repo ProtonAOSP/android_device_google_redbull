64fb71593e145ed104e15b97f59a8e61e3f69769
David Anderson <dvander@google.com>
2021-02-16 12:09:30 -0800

    Enable VABC on a per-device basis.
    Bug: 180253982
    Test: VABC enabled on redfin, bramble
          VABC disabled on barbet
    Change-Id: I4b7c0bd8fce817ae99db1adad50d6c0a38567807
    (cherry picked from commit 8289e036ef80850b60231477e8b0aa4d941ae02e)

--- device-common.mk
+++ device-common.mk
@@ -954,8 +954,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
 # Enable watchdog timeout loop breaker.
 PRODUCT_PROPERTY_OVERRIDES += \
     framework_watchdog.fatal_window.second=600 \
