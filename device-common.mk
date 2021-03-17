2d9851503d736e9feb07c94d4ba9eec8474442dc
Wilson Sung <wilsonsung@google.com>
2021-03-17 16:24:48 +0000

    Exclude allowlist in without-vendor branch
    Bug: 182997128
    Change-Id: I3b3997dee45d9952ec5c55ee70649788c06e254c

--- device-common.mk
+++ device-common.mk
@@ -1016,7 +1016,9 @@ include hardware/google/pixel/citadel/citadel.mk
 # Pixel Logger
 include hardware/google/pixel/PixelLogger/PixelLogger.mk
 
+ifneq ($(wildcard vendor/),)
 -include $(LOCAL_PATH)/redbull_allowlist.mk
+endif
 #################################################################################
 
 # Set system properties identifying the chipset
