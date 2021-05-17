bc9c205bc9e268b84e614ac38152c57dc4478d05
Jenny Ho <hsiufangho@google.com>
2021-05-18 07:46:40 +0800

    device-common: set testing_battery_profile default 2
    set 2 to prevent from limiting battery level in unexpected testing situation
    
    Bug: 180511460
    Signed-off-by: Jenny Ho <hsiufangho@google.com>
    Change-Id: I24160f30f9ab3e4f3f0ae33ddce11ac1f1dc4bd2

--- device-common.mk
+++ device-common.mk
@@ -71,7 +71,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.debug.sensors.accel_cal=1
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.vendor.testing_battery_profile=0
+    persist.vendor.testing_battery_profile=2
 
 # The default value of this variable is false and should only be set to true when
 # the device allows users to retain eSIM profiles after factory reset of user data.
