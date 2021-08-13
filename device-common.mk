4bbf8559056d66adfd8c8fb01656e8c0fd4cc799
Paul Scovanner <pscovanner@google.com>
2021-08-13 20:31:35 +0000

    Update Redbull SVN to 31
    Bug: 193547118
    Change-Id: I83b9c6938c0a30666a29940608e8c0be24aacbdc
    Merged-In: I0f122c5f297f3bb7f3e84e70fa5c779041a0fb7a

--- device-common.mk
+++ device-common.mk
@@ -841,7 +841,7 @@ PRODUCT_PACKAGES += $(HIDL_WRAPPER)
 # Increment the SVN for any official public releases
 ifeq ($(PRODUCT_DEVICE_SVN_OVERRIDE),)
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.build.svn=30
+	ro.vendor.build.svn=31
 endif
 
 # Enable iwlan service logging for debug
